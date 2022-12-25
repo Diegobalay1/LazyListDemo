5a35ab03b9fa90a1fd0e29fbfaa4c38647483421
balay <dlolhd@gmail.com>
2022-12-25 14:13:17 +0100

    Handling image loading and Designing the list item composable.

--- app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -3,17 +3,31 @@ package com.example.lazylistdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.lazylistdemo.ui.theme.LazyListDemoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var itemArray: Array<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        itemArray = resources.getStringArray(R.array.car_array)
+
         super.onCreate(savedInstanceState)
         setContent {
             LazyListDemoTheme {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainScreen()
+                    MainScreen(itemArray = itemArray as Array<out String>)
                 }
             }
         }
@@ -30,14 +44,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(itemArray: Array<out String>) {
+    //ImageLoader(item = "Plymouth GTX")
+    MyListItem(item = "Buick Roadmaster")
+}
 
+// Coil - images
+/* Example
+   AsyncImage(
+       model = "https://example.com/image.jpg",
+       contentDescription = null
+   )
+ */
+@Composable
+fun ImageLoader(item: String) {
+
+    val url = "https://www.ebookfrenzy.com/book_examples/car_logos/" + item.substringBefore(" ") + "_logo.png"
+
+    Image(
+        painter = rememberAsyncImagePainter(url),
+        contentDescription = "car image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
+    )
+}
+
+@Composable
+fun MyListItem(item: String) {
+    Card(
+        Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            ImageLoader(item = item)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = item,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+
+    val itemArray: Array<String> = arrayOf("Cadillac Eldorado",
+                                        "Ford Fairlane", "Plymouth Fury")
+
     LazyListDemoTheme {
-        MainScreen()
+        MainScreen(itemArray = itemArray)
     }
 }
+
+
+
+
+
+
+
+
