0028e6c443970a198dbbb69ed1d751bf95a08618
balay <dlolhd@gmail.com>
2022-12-25 17:06:27 +0100

    Making list items clickable.

--- app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.lazylistdemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,10 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.example.lazylistdemo.ui.theme.LazyListDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,9 +51,20 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(itemArray: Array<out String>) {
     //ImageLoader(item = "Plymouth GTX")
     //MyListItem(item = "Buick Roadmaster")
+
+    val context = LocalContext.current
+
+    val onListItemClick = { text: String ->
+        Toast.makeText(
+            context,
+            text,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
     LazyColumn {
         items(itemArray) { model ->
-            MyListItem(item = model)
+            MyListItem(item = model, onItemClick = onListItemClick)
         }
     }
 }
@@ -77,11 +90,12 @@ fun ImageLoader(item: String) {
 }
 
 @Composable
-fun MyListItem(item: String) {
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
     Card(
         Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
         shape = RoundedCornerShape(10.dp),
         elevation = 5.dp
     ) {
