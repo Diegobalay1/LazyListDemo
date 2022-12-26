65ddae4356e9e085194b7fa34182a182c67075b0
balay <dlolhd@gmail.com>
2022-12-26 10:52:40 +0100

    Displaying the headers and items and Adding sticky headers.

--- app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,6 +19,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,6 +50,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(itemArray: Array<out String>) {
     //ImageLoader(item = "Plymouth GTX")
@@ -64,8 +68,23 @@ fun MainScreen(itemArray: Array<out String>) {
     }
 
     LazyColumn {
-        items(itemArray) { model ->
-            MyListItem(item = model, onItemClick = onListItemClick)
+        groupedItems.forEach { (manufacturer, models) ->
+
+            stickyHeader {
+                Text(
+                    text = manufacturer,
+                    color = Color.White,
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                )
+            }
+
+            //items(itemArray) { model ->
+            items(models) { model ->
+                MyListItem(item = model, onItemClick = onListItemClick)
+            }
         }
     }
 }
