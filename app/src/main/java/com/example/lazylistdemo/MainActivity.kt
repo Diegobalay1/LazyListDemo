f3dd59f99bd73290449f68fbb6c9d8d61158e625
balay <dlolhd@gmail.com>
2022-12-26 11:01:55 +0100

    Reacting to scroll position.

--- app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -56,6 +58,10 @@ fun MainScreen(itemArray: Array<out String>) {
     //ImageLoader(item = "Plymouth GTX")
     //MyListItem(item = "Buick Roadmaster")
 
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5
+
     val context = LocalContext.current
     val groupedItems = itemArray.groupBy { it.substringBefore(' ') }
 
@@ -67,26 +73,32 @@ fun MainScreen(itemArray: Array<out String>) {
         ).show()
     }
 
-    LazyColumn {
-        groupedItems.forEach { (manufacturer, models) ->
+    Box() {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 40.dp)
+        ) {
+            groupedItems.forEach { (manufacturer, models) ->
 
-            stickyHeader {
-                Text(
-                    text = manufacturer,
-                    color = Color.White,
-                    modifier = Modifier
-                        .background(Color.Gray)
-                        .padding(5.dp)
-                        .fillMaxWidth()
-                )
-            }
+                stickyHeader {
+                    Text(
+                        text = manufacturer,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(Color.Gray)
+                            .padding(5.dp)
+                            .fillMaxWidth()
+                    )
+                }
 
-            //items(itemArray) { model ->
-            items(models) { model ->
-                MyListItem(item = model, onItemClick = onListItemClick)
+                //items(itemArray) { model ->
+                items(models) { model ->
+                    MyListItem(item = model, onItemClick = onListItemClick)
+                }
             }
         }
     }
+
 }
 
 // Coil - images
