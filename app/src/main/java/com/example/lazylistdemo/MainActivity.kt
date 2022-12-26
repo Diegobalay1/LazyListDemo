754bb5c6e81c3d8d86d9406c6da47435588d97d4
balay <dlolhd@gmail.com>
2022-12-26 11:13:16 +0100

    Adding the scroll button.

--- app/src/main/java/com/example/lazylistdemo/MainActivity.kt
+++ app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -28,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.lazylistdemo.ui.theme.LazyListDemoTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -76,7 +72,7 @@ fun MainScreen(itemArray: Array<out String>) {
     Box() {
         LazyColumn(
             state = listState,
-            contentPadding = PaddingValues(bottom = 40.dp)
+            contentPadding = PaddingValues(bottom = 45.dp)
         ) {
             groupedItems.forEach { (manufacturer, models) ->
 
@@ -97,7 +93,28 @@ fun MainScreen(itemArray: Array<out String>) {
                 }
             }
         }
-    }
+
+        AnimatedVisibility(
+            visible = displayButton,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Top")
+            }
+        }
+
+    }//end Box
 
 }
 
